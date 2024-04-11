fa4934ff9199f9c3b6045787345f49fdf604b51b
Nikita Romantsev <atikinvestnamor@gmail.com>
2024-04-11 06:53:45 +0300

    initial commit

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,18 @@
+package runner;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.ConfigurationParameters;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.core.options.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@ConfigurationParameters({
+        @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features"),
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps"),
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
+})
+public class TestRunner {
+}
--- src/test/java/steps/WarePageTest.java
+++ src/test/java/steps/WarePageTest.java
@@ -1,2 +1,47 @@
-package steps;public class WarePageTest {
+package steps;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.ru.И;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class WarePageTest {
+
+    private static WebDriver webDriver;
+
+    @Before
+    public void before() {
+        webDriver = new ChromeDriver();
+    }
+
+    @И("тестовый стенд запущен и открыта страница {string}")
+    public void тестовый_стенд_запущен_и_открыта_страница(String string) {
+        webDriver.get(string);
+        webDriver.manage().window().maximize();
+    }
+
+    @И("открыта страница с товарами")
+    public void открыта_страница_с_товарами() {
+        WebElement dropDownBar = webDriver.findElement(By.id("navbarDropdown"));
+        dropDownBar.click();
+        WebElement waresButton = webDriver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[2]/div/a[1]"));
+        waresButton.click();
+    }
+
+    @И("нажимаем на кнопку добавить")
+    public void нажимаем_на_кнопку_добавить() {
+        WebElement addBtn = webDriver.findElement(By.xpath("/html/body/div/div[2]/div/div[1]/button"));
+        addBtn.click();
+    }
+
+    @И("поле Наименование заполняется значением {string}")
+    public void поле_наименование_заполняется_значением(String string) {
+        WebElement nameField = webDriver.findElement(By.id("name"));
+        nameField.click();
+        nameField.sendKeys(string);
+    }
 }
